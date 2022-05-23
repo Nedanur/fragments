95f832f96728bcf6aa2c550c2379804814565150
Neda Basoglu <nedabsgl@gmail.com>
2022-05-23 19:55:48 -0400

    Initial update on src folder

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+
 // src/app.js
 
 const express = require('express');
@@ -6,7 +7,6 @@ const helmet = require('helmet');
 const compression = require('compression');
 
 // version and author from our package.json file
-const { version, author } = require('../package.json');
 
 const logger = require('./logger');
 const pino = require('pino-http')({
@@ -26,24 +26,14 @@ app.use(helmet());
 // Use CORS middleware so we can make requests across origins
 app.use(cors());
 
+
 // Use gzip/deflate compression middleware
 app.use(compression());
 
 // Define a simple health check route. If the server is running
 // we'll respond with a 200 OK.  If not, the server isn't healthy.
-app.get('/', (req, res) => {
-  // Clients shouldn't cache this response (always request it fresh)
-  // See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching#controlling_caching
-  res.setHeader('Cache-Control', 'no-cache');
 
-  // Send a 200 'OK' response with info about our repo
-  res.status(200).json({
-    status: 'ok',
-    author,
-    githubUrl: 'https://github.com/Nedanur/fragments',
-    version,
-  });
-});
+app.use('/', require('./routes'));
 
 // Add 404 middleware to handle any requests for resources that can't be found
 app.use((req, res) => {
@@ -80,3 +70,5 @@ app.use((err, req, res, next) => {
 
 // Export our `app` so we can access it in server.js
 module.exports = app;
+
+
